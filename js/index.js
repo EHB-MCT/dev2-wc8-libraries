ffc650d51edb92c415bce0946dbeec1ad960c13f
Joni De Borger <joni.de.borger@ehb.be>
2024-04-23 14:55:26 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let map; // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+
+function init() {
+    // initialise de kaart
+
+    // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
+    // vergeet openstreetmap attributie niet
+
+    // gebruik de functie "loadMarkers" om de markers toe te voegen
+}
+
+function loadMarkers() {
+    // fetch de data van opendata.brussels.be
+    // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+
+}
+
+function addMarker(lat, lon) {
+    // voeg een marker toe op lat, lon
+
+}
